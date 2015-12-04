27fa692fe89eaa3be443b7c1da47a03b85d7fbb6
michael <lolidunno@7clams.org>
2015-12-04 14:23:40 -0800

    Define STATIC_ROOT

--- superlists/settings.py
+++ superlists/settings.py
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'superlists.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,3 +104,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
