3e762dc9a1212f8b05364b888b6497062051220e
michael <lolidunno@7clams.org>
2015-12-09 23:21:30 -0800

    Use form in home_page.

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from lists.forms import ItemForm
 from lists.forms import EMPTY_ITEM_ERROR
 
+
 class ItemFormTest(TestCase):
 
     def test_form_renders_item_text_input(self):
